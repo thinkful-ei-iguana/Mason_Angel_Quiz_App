bdeeb38d5b48165587dbd484ecd2a08b9a72da01
Mason Reichbauer <mreichba@mail.usf.edu>
2019-10-25 22:30:26 -0400

    added STORE

--- index.js
+++ index.js
@@ -1,10 +1,59 @@
+'use strict';
+const STORE = [
+  {//Q1
+    Questions: 'What is Keanu Reaves cyber alter-ego in The Matrix movies?',
+    Options: [
+      'Trinity',
+      'Neo',
+      'Mr. Anderson',
+      'Morpheus'
+    ],
+    Answers: 'Neo'
+  },
+  {//Q2
+    Questions: 'Which colored pill does Neo take to go "down the rabbit hole"?',
+    Options: [
+      'Blue',
+      'Red',
+      'Yellow',
+      'Green'
+    ],
+    Answers: 'Red'   
+  },
+  {//Q3
+    Questions: 'Who rescues Neo from the Matrix in the first movie?',
+    Options: [
+      'Cypher',
+      'Trinity',
+      'Morpheus',
+      'Mr. Anderson'
+    ],
+    Answers: 'Morpheus'
+  },
+  {//Q4
+    Questions: 'Where does the last of the natural born human population on earth live?',
+    Options: [
+      'Babylon',
+      'Zion',
+      'Crete',
+      'L.A.'
+    ],
+    Answers: 'Zion'
+  },
+  {//Q5
+    Questions: 'Who is "The One"?',
+    Options:[
+      'Trinity',
+      'Morpheus',
+      'The Oracle',
+      'Neo'
+    ],
+    Answers: 'Neo'
+  },
+];
+
 $(document).ready(function(){
   
 
 
-})
-
-const STORE =[{
-
-}
-]
+});
